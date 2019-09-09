8d3889f7c399f692f2d79ef52ee62780b27c42b4
Stefano Sartor <sartor.stefano@gmail.com>
2019-09-09 11:02:54 +0200

    updated docker_run script

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,7 @@
 docker run -ti -e DISPLAY\
        -v /tmp/.X11-unix:/tmp/.X11-unix\
        -v $HOME/:/home/user/:rw\
+       -v $PWD:/striptease:rw\
+       -w /striptease\
        -u user\
        --net=host strip/striptease bash
