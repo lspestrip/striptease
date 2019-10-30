bf85d6798ee0395c8d5a11ebb3df4ccd8f4c9bfa
Maurizio Tomasi <ziotom78@gmail.com>
2019-10-30 16:58:33 +0100

    Produce better error messages

--- TestRunner/stripconnection.cpp
+++ TestRunner/stripconnection.cpp
@@ -74,11 +74,14 @@ void StripConnection::commandCompleted()
     if(answer.isObject())
     {
         QJsonObject answerObj(answer.object());
-        if(!answerObj.contains("status") ||
-                answerObj["status"].toString().toUpper() != "OK")
+        if(!answerObj.contains("status")) {
+        }
+
+        QString status{answerObj["status"].toString()};
+        if(status.toUpper() != "OK")
         {
             errorCondition = true;
-            emit error(QString("No status code in the JSON returned by the server"));
+            emit error(QString("The server returned an error status: %1").arg(status));
         } else {
             emit success();
         }
