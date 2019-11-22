e544048716d6851e8f65061dd9896c5d54463687
Maurizio Tomasi <ziotom78@gmail.com>
2019-11-22 13:57:40 +0100

    Make the turn-on procedure more modular

--- program_turnon/__init__.py
+++ program_turnon/__init__.py
@@ -3,6 +3,8 @@ from web.rest.base import Connection
 from config import Config
 from copy import deepcopy
 from collections import namedtuple
+from datetime import datetime
+from striptease import StripTag
 import logging as log
 import re
 import time
@@ -11,6 +13,7 @@ import sys
 import pandas as pd
 from pprint import pprint
 from striptease.biases import InstrumentBiases, BoardCalibration
+from striptease.procedures import StripProcedure
 
 CalibrationCurve = namedtuple("CalibrationCurve", [
     "slope",
@@ -171,7 +174,7 @@ class SetupBoard(object):
                                 (x, i) for (i, x) in enumerate(cur_board["pols"])
                             ]
                         break
-                    except (exc):
+                    except Exception as exc:
                         log.warning(
                             f'No suitable bias file for board "{board_name}" found: {exc}'
                         )
@@ -462,6 +465,251 @@ class SetupBoard(object):
 
         if not self.post_command(url, cmd):
             return
+
+
+def biases_to_str(biases):
+    return "Biases: " + ",".join(
+        [
+            str(biases.vd0),
+            str(biases.vd1),
+            str(biases.vd2),
+            str(biases.vd3),
+            str(biases.vd4),
+            str(biases.vd5),
+            str(biases.vg0),
+            str(biases.vg1),
+            str(biases.vg2),
+            str(biases.vg3),
+            str(biases.vg4),
+            str(biases.vg5),
+            str(biases.vg4a),
+            str(biases.vg5a),
+            str(biases.vpin0),
+            str(biases.vpin1),
+            str(biases.vpin2),
+            str(biases.vpin3),
+            str(biases.ipin0),
+            str(biases.ipin1),
+            str(biases.ipin2),
+            str(biases.ipin3),
+            str(biases.id0),
+            str(biases.id1),
+            str(biases.id2),
+            str(biases.id3),
+            str(biases.id4),
+            str(biases.id5),
+        ]
+    )
+
+
+class TurnOnOffProcedure(StripProcedure):
+    def __init__(self, waittime_s=5, turnon=True):
+        super(TurnOnOffProcedure, self).__init__()
+        self.board = None
+        self.horn = None
+        self.polarimeter = None
+        self.waittime_s = waittime_s
+        self.turnon = turnon
+
+    def set_board_horn_polarimeter(self, new_board, new_horn, new_pol=None):
+        self.board = new_board
+        self.horn = new_horn
+        self.polarimeter = new_pol
+
+    def run(self):
+        "Depending on `self.turnon`, execute a turn-on or turn-off procedure for `self.horn`."
+        if self.turnon:
+            self.run_turnon()
+        else:
+            self.run_turnoff()
+
+    def run_turnon(self, turn_on_board=True, stable_acquisition_time_s=120):
+        """Execute a turn-on procedure for the horn specified in `self.horn`.
+        
+        Optional parameters:
+        
+        - turn_on_board: if True, turn on the board. Set it to false if you are sure the
+            board has already been turned on (default: True).
+
+        - stable_acquisition_time_s: if nonzero, wait for the specified amount of
+            seconds once the polarimeter has been fully turned on.                         
+        """
+
+        assert self.horn
+        board_setup = SetupBoard(
+            config=self.conf, board_name=self.board, post_command=self.command_emitter
+        )
+
+        current_time = datetime.now().strftime("%A %Y-%m-%d %H:%M:%S (%Z)")
+        board_setup.log(
+            f"Here begins the turnon procedure for polarimeter {self.horn}, "
+            + f"created on {current_time} using program_turnon.py"
+        )
+        board_setup.log(f"We are using the setup for board {self.board}")
+        if self.polarimeter:
+            board_setup.log(
+                f"This procedure assumes that horn {self.horn} is connected to polarimeter {self.polarimeter}"
+            )
+
+        # 1
+        if turn_on_board:
+            with StripTag(
+                conn=self.command_emitter,
+                name="BOARD_TURN_ON",
+                comment=f"Turning on board for {self.horn}",
+            ):
+                board_setup.log("Going to set up the board…")
+                board_setup.board_setup()
+                board_setup.log("Board has been set up")
+
+        # 2
+        with StripTag(
+            conn=self.command_emitter,
+            name=f"ELECTRONICS_ENABLE_{self.horn}",
+            comment=f"Enabling electronics for {self.horn}",
+        ):
+            board_setup.log(f"Enabling electronics for {self.horn}…")
+            board_setup.enable_electronics(polarimeter=self.horn)
+            board_setup.log("The electronics has been enabled")
+
+        # 3
+        for idx in (0, 1, 2, 3):
+            with StripTag(
+                conn=self.command_emitter,
+                name=f"DETECTOR_TURN_ON_{self.horn}_{idx}",
+                comment=f"Turning on detector {idx} in {self.horn}",
+            ):
+                board_setup.turn_on_detector(self.horn, idx)
+
+        # 4
+        if self.polarimeter:
+            biases = self.biases.get_biases(polarimeter_name=self.polarimeter)
+            board_setup.log(f"{self.polarimeter}: {biases_to_str(biases)}")
+        else:
+            biases = self.biases.get_biases(module_name=self.horn)
+            board_setup.log(f"{self.horn}: {biases_to_str(biases)}")
+
+        for (index, vpin, ipin) in zip(
+            range(4),
+            [biases.vpin0, biases.vpin1, biases.vpin2, biases.vpin3],
+            [biases.ipin0, biases.ipin1, biases.ipin2, biases.ipin3],
+        ):
+            try:
+                with StripTag(
+                    conn=self.command_emitter,
+                    name=f"PHSW_BIAS_{self.horn}_{index}",
+                    comment=f"Setting biases for PH/SW {index} in {self.horn}",
+                ):
+                    board_setup.set_phsw_bias(self.horn, index, vpin, ipin)
+            except:
+                log.warning(f"Unable to set bias for detector #{index}")
+
+        # 5
+        for idx in (0, 1, 2, 3):
+            with StripTag(
+                conn=self.command_emitter,
+                name=f"PHSW_STATUS_{self.horn}_{idx}",
+                comment=f"Setting status for PH/SW {idx} in {self.horn}",
+            ):
+                board_setup.set_phsw_status(self.horn, idx, status=7)
+
+        # 6
+        for lna in ("HA3", "HA2", "HA1", "HB3", "HB2", "HB1"):
+            for step_idx, cur_step in enumerate([0.0, 0.5, 1.0]):
+                with StripTag(
+                    conn=self.command_emitter,
+                    name=f"VD_SET_{self.horn}_{lna}",
+                    comment=f"Setting drain voltages for LNA {lna} in {self.horn}",
+                ):
+                    board_setup.setup_VD(self.horn, lna, step=cur_step)
+
+                    if step_idx == 0:
+                        board_setup.setup_VG(self.horn, lna, step=1.0)
+
+                    if False and cur_step == 1.0:
+                        # In mode 5, the following command should be useless…
+                        board_setup.setup_ID(self.horn, lna, step=1.0)
+
+                if self.waittime_s > 0:
+                    with StripTag(
+                        conn=self.command_emitter,
+                        name=f"VD_SET_{self.horn}_{lna}_ACQUISITION",
+                        comment=f"Acquiring some data after VD_SET_{lna}",
+                    ):
+                        self.wait(seconds=self.waittime_s)
+
+        if stable_acquisition_time_s > 0:
+            board_setup.log(
+                f"Horn {self.horn} has been turned on, stable acquisition for {stable_acquisition_time_s} s begins here"
+            )
+            with StripTag(
+                conn=self.command_emitter, name=f"STABLE_ACQUISITION_{self.horn}"
+            ):
+                self.wait(seconds=stable_acquisition_time_s)
+            board_setup.log(f"End of stable acquisition for horn {self.horn}")
+        else:
+            board_setup.log(f"Horn {self.horn} has been turned on")
+
+    def run_turnoff(self):
+        "Execute a turn-off procedure for the horn specified in `self.horn`."
+
+        assert self.horn
+        board_setup = SetupBoard(
+            config=self.conf, board_name=self.board, post_command=self.command_emitter
+        )
+
+        current_time = datetime.now().strftime("%A %Y-%m-%d %H:%M:%S (%Z)")
+        board_setup.log(
+            f"Here begins the turnoff procedure for polarimeter {self.horn}, "
+            + f"created on {current_time} using program_turnon.py"
+        )
+        board_setup.log(f"We are using the setup for board {self.board}")
+        if self.polarimeter:
+            board_setup.log(
+                f"This procedure assumes that horn {self.horn} is connected to polarimeter {self.polarimeter}"
+            )
+
+        # 1
+        with StripTag(
+            conn=self.command_emitter,
+            name="BOARD_TURN_OFF",
+            comment=f"Turning off board for {self.horn}",
+        ):
+            board_setup.log("Going to set up the board…")
+            board_setup.board_setup()
+            board_setup.log("Board has been set up")
+
+        # 6
+        for lna in reversed(["HA3", "HA2", "HA1", "HB3", "HB2", "HB1"]):
+            for step_idx, cur_step in enumerate(reversed([0.0, 0.5, 1.0])):
+                with StripTag(
+                    conn=self.command_emitter,
+                    name="VD_SET",
+                    comment=f"Setting drain voltages for LNA {lna} in {self.horn}",
+                ):
+                    board_setup.setup_VD(self.horn, lna, step=cur_step)
+
+                    if step_idx == 0:
+                        board_setup.setup_VG(self.horn, lna, step=1.0)
+
+                    if False and cur_step == 1.0:
+                        # In mode 5, the following command should be useless…
+                        board_setup.setup_ID(self.horn, lna, step=1.0)
+
+                if self.waittime_s > 0:
+                    self.wait(seconds=self.waittime_s)
+
+        # 2
+        with StripTag(
+            conn=self.command_emitter,
+            name="ELECTRONICS_DISABLE",
+            comment=f"Enabling electronics for {self.horn}",
+        ):
+            board_setup.log(f"Disabling electronics for {self.horn}…")
+            board_setup.disable_electronics(polarimeter=self.horn)
+            board_setup.log("The electronics has been disabled")
+
+        board_setup.log(f"Turnoff procedure for {self.horn} completed")
 
 
 if __name__ == "__main__":
