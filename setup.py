ac1a2897c8ae5348c31d3d6e8be45f9c163d56f6
Maurizio Tomasi <ziotom78@gmail.com>
2021-08-19 19:15:09 +0200

    Add data files to source distribution and fix docs

--- setup.py
+++ setup.py
@@ -11,4 +11,28 @@ setup(
     author_email="",
     url="https://github.com/lspestrip/striptease",
     packages=find_packages(),
+    data_files=[
+        (
+            "data",
+            [
+                "data/bias1_cal.xlsx",
+                "data/bias2_cal.xlsx",
+                "data/bias3_cal.xlsx",
+                "data/bias4_cal.xlsx",
+                "data/bias5_cal.xlsx",
+                "data/bias6_cal.xlsx",
+                "data/bias7_cal.xlsx",
+                "data/bias8_cal.xlsx",
+                "data/bias9_cal.xlsx",
+                "data/cold_bias_table.json",
+                "data/default_biases_warm_nominal_you_want_to_use_this_when_everything_will_be_fixed.xlsx",
+                "data/default_biases_warm.xlsx",
+                "data/hk_pars_BOARD_BIAS.csv",
+                "data/hk_pars_BOARD_DAQ.csv",
+                "data/hk_pars_POL_BIAS.csv",
+                "data/hk_pars_POL_DAQ.csv",
+                "data/input_bias_IVtest.xlsx",
+            ],
+        )
+    ],
 )
