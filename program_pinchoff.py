6efba1b118793375ef1198e4a4426fc25fab393f
Maurizio Tomasi <ziotom78@gmail.com>
2021-10-18 11:32:00 +0200

    Take advantage of wait_with_tag and polarimeter_iterator

--- program_pinchoff.py
+++ program_pinchoff.py
@@ -4,7 +4,12 @@
 import logging as log
 
 from calibration import CalibrationTables
-from striptease import STRIP_BOARD_NAMES, BOARD_TO_W_BAND_POL, StripTag
+from striptease import (
+    STRIP_BOARD_NAMES,
+    StripTag,
+    polarimeter_iterator,
+    wait_with_tag,
+)
 from striptease.procedures import StripProcedure
 from program_turnon import TurnOnOffProcedure
 
@@ -17,17 +22,9 @@ class PinchOffProcedure(StripProcedure):
         log.info(f"Turnon of board {board}")
         turnon_proc = TurnOnOffProcedure(waittime_s=1.0, turnon=True)
 
-        for cur_horn_idx in range(8):
-            if board == "I" and cur_horn_idx == 7:
-                continue
-
-            if cur_horn_idx != 7:
-                polname = f"{board}{cur_horn_idx}"
-            else:
-                polname = BOARD_TO_W_BAND_POL[board]
-
+        for _, pol_idx, pol_name in polarimeter_iterator(boards=board):
             turnon_proc.set_board_horn_polarimeter(
-                new_board=board, new_horn=polname, new_pol=None
+                new_board=board, new_horn=pol_name, new_pol=None
             )
             turnon_proc.run()
 
@@ -45,39 +42,32 @@ class PinchOffProcedure(StripProcedure):
             self.wait(seconds=5)
 
         # Verification step
-        with StripTag(conn=self.command_emitter, name="PINCHOFF_VERIFICATION_1"):
-            self.wait(seconds=300)
+        wait_with_tag(
+            conn=self.command_emitter, name="PINCHOFF_VERIFICATION_1", seconds=300
+        )
 
         for cur_board in STRIP_BOARD_NAMES:
             # Now run the pinch-off procedure for each board
             with StripTag(conn=self.command_emitter, name=f"PINCHOFF_TILE_{cur_board}"):
-                for cur_horn_idx in range(8):
-                    if cur_board == "I" and cur_horn_idx == 7:
-                        continue
-
-                    if cur_horn_idx == 7:
-                        cur_horn_name = BOARD_TO_W_BAND_POL[cur_board]
-                    else:
-                        cur_horn_name = f"{cur_board}{cur_horn_idx}"
-
-                    self.conn.enable_electronics(polarimeter=cur_horn_name, pol_mode=5)
+                for _, pol_idx, pol_name in polarimeter_iterator(boards=cur_board):
+                    self.conn.enable_electronics(polarimeter=pol_name, pol_mode=5)
 
                     for id_value_muA in (100, 4_000, 8_000, 12_000):
                         for cur_lna in ("HA3", "HA2", "HA1", "HB3", "HB2", "HB1"):
                             # Convert the current (μA) in ADU
                             adu = calibr.physical_units_to_adu(
-                                polarimeter=cur_horn_name,
+                                polarimeter=pol_name,
                                 hk="idrain",
                                 component=cur_lna,
                                 value=id_value_muA,
                             )
-                            self.conn.set_id(cur_horn_name, cur_lna, value_adu=adu)
+                            self.conn.set_id(pol_name, cur_lna, value_adu=adu)
 
-                            with StripTag(
+                            wait_with_tag(
                                 conn=self.command_emitter,
-                                name=f"PINCHOFF_IDSET_{cur_horn_name}_{cur_lna}_{id_value_muA:.0f}muA",
-                            ):
-                                self.conn.wait(seconds=18)
+                                name=f"PINCHOFF_IDSET_{pol_name}_{cur_lna}_{id_value_muA:.0f}muA",
+                                seconds=18,
+                            )
 
 
 if __name__ == "__main__":
