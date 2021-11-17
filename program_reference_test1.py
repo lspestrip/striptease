b74a44264d0497ba8f3d150c7ffccdb299036a70
Maurizio Tomasi <ziotom78@gmail.com>
2021-11-17 10:56:04 +0100

    Add Amalia's scripts

---
+++ program_reference_test1.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+# Procedura 1
+
+import logging as log
+from calibration import CalibrationTables
+from reference_test import (
+    proc_1,
+)
+from striptease import (
+    STRIP_BOARD_NAMES,
+    polarimeter_iterator,
+    StripProcedure,
+    StripTag,
+)
+from program_turnon import TurnOnOffProcedure
+
+
+class ReferenceTestProcedure(StripProcedure):
+    def __init__(self):
+        super(ReferenceTestProcedure, self).__init__()
+        self.calib = CalibrationTables()
+
+    def run(self):
+        turnon_proc = TurnOnOffProcedure(waittime_s=1.0, turnon=True)
+        turnoff_proc = TurnOnOffProcedure(waittime_s=1.0, turnon=False)
+
+        for cur_board, pol_idx, polname in polarimeter_iterator(args.board):
+            self.conn.log(message=f"turning on {polname} for reference procedure…")
+            with StripTag(conn=self.command_emitter, name=f"ref_turnon_pol_{polname}"):
+
+                turnon_proc.set_board_horn_polarimeter(
+                    new_board=cur_board,
+                    new_horn=polname,
+                    new_pol=None,
+                )
+                turnon_proc.run()
+                self.command_emitter.command_list += turnon_proc.get_command_list()
+                turnon_proc.clear_command_list()
+
+            self.conn.log(message=f"{polname} is now on, start the reference procedure")
+            proc_1(self, polname, cur_board)
+            self.conn.log(
+                message=f"reference procedure for {polname} has been completed, turning {polname} off…"
+            )
+
+            with StripTag(conn=self.command_emitter, name=f"ref_turnoff_pol_{polname}"):
+                # turn off polarimeter
+                turnoff_proc.set_board_horn_polarimeter(
+                    new_board=cur_board,
+                    new_horn=polname,
+                    new_pol=None,
+                )
+                turnoff_proc.run()
+                self.command_emitter.command_list += turnoff_proc.get_command_list()
+                turnoff_proc.clear_command_list()
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser, RawDescriptionHelpFormatter
+
+    parser = ArgumentParser(
+        description="Procedure a command sequence to turn on the boards",
+        formatter_class=RawDescriptionHelpFormatter,
+        epilog=""""
+
+        python3 amalia_reference.py
+        """,
+    )
+    parser.add_argument(
+        "--output",
+        "-o",
+        metavar="FILENAME",
+        type=str,
+        dest="output_filename",
+        default="",
+        help="Name of the file where to write the output (in JSON format)."
+        "If not provided, the output will be sent to stdout",
+    )
+
+    parser.add_argument(
+        "board",
+        type=str,
+        nargs="?",
+        default=STRIP_BOARD_NAMES,
+        help="ref_turn on one or more boards",
+    )
+
+    args = parser.parse_args()
+
+    log.basicConfig(level=log.INFO, format="[%(asctime)s %(levelname)s]%(message)s")
+
+    proc = ReferenceTestProcedure()
+    proc.run()
+    proc.output_json(args.output_filename)
---
+++ reference_test/__init__.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+from striptease import (
+    PhswPinMode,
+    StripTag,
+    wait_with_tag,
+)
+from program_turnon import SetupBoard
+
+
+def set_0_bias(self, polname):
+    with StripTag(conn=self.command_emitter, name=f"ref_set_0B_pol_{polname}"):
+
+        for cur_lna in ("HA3", "HA2", "HA1", "HB3", "HB2", "HB1"):
+            adu = self.calib.physical_units_to_adu(
+                polarimeter=polname,
+                hk="vdrain",
+                component=cur_lna,
+                value=0,
+            )
+            self.conn.set_vd(polname, cur_lna, value_adu=adu)
+
+        self.conn.set_pol_mode(polarimeter=polname, mode=5)
+
+
+def proc_1(self, polname, cur_board):
+    # set to zero bias
+    set_0_bias(self, polname)
+    self.conn.set_hk_scan(boards=cur_board, allboards=False, time_ms=500)
+    wait_with_tag(conn=self.conn, seconds=120, name=f"ref_acquisition_0B_pol{polname}")
+
+    self.conn.log(message="ref_set pol state to unsw 1010")
+    # set pol state to unsw 1010 (STATO 1)
+    with StripTag(conn=self.command_emitter, name=f"ref_set_pol_state{polname}"):
+        for h, s in enumerate(
+            [
+                PhswPinMode.STILL_NO_SIGNAL,
+                PhswPinMode.STILL_SIGNAL,
+                PhswPinMode.STILL_NO_SIGNAL,
+                PhswPinMode.STILL_SIGNAL,
+            ]
+        ):
+            self.conn.set_phsw_status(polarimeter=polname, phsw_index=h, status=s)
+
+    self.conn.set_hk_scan(boards=cur_board, allboards=False, time_ms=500)
+
+    # imposto un wait per l'acquisizione
+    wait_with_tag(
+        conn=self.conn, seconds=120, name=f"ref_acquisition_STATE1_pol{polname}"
+    )
+
+    # set pol to default bias
+    board_setup = SetupBoard(
+        config=self.conf, post_command=self.command_emitter, board_name=cur_board
+    )
+
+    self.conn.log(message="ref_set pol state to default bias")
+    for lna in ("HA3", "HA2", "HA1", "HB3", "HB2", "HB1"):
+        board_setup.setup_VD(polname, lna, step=1.0)
+        board_setup.setup_VG(polname, lna, step=1.0)
+
+    self.conn.set_hk_scan(boards=cur_board, allboards=False, time_ms=500)
+
+    wait_with_tag(conn=self.conn, seconds=120, name=f"ref_acquisition_pol{polname}")
+
+    self.conn.log(message="ref_set pol state to unsw 0101")
+    # set pol to unsw 0101 (STATO 2)
+    with StripTag(conn=self.command_emitter, name=f"ref_set_pol_state{polname}"):
+        for h, s in enumerate(
+            [
+                PhswPinMode.STILL_SIGNAL,
+                PhswPinMode.STILL_NO_SIGNAL,
+                PhswPinMode.STILL_SIGNAL,
+                PhswPinMode.STILL_NO_SIGNAL,
+            ]
+        ):
+            self.conn.set_phsw_status(polarimeter=polname, phsw_index=h, status=s)
+
+    self.conn.set_hk_scan(boards=cur_board, allboards=False, time_ms=200)
+
+    wait_with_tag(
+        conn=self.conn, seconds=120, name=f"ref_acquisition_STATE2_pol{polname}"
+    )
+
+    self.conn.log(message="ref_set phsw state to default bias")
+    # set phsw modulation to default bias
+    for h in range(4):
+        with StripTag(conn=self.command_emitter, name=f"ref_set_pol_state{polname}"):
+            self.conn.set_phsw_status(
+                polarimeter=polname,
+                phsw_index=h,
+                status=PhswPinMode.DEFAULT_STATE,
+            )
+
+    self.conn.set_hk_scan(boards=cur_board, allboards=False, time_ms=500)
+
+    wait_with_tag(conn=self.conn, seconds=120, name=f"ref_acquisition_pol{polname}")
+
+    self.conn.log(message="ref_set phsw state to antidefault bias")
+    # set phsw modulation to antidefault bias
+    with StripTag(conn=self.command_emitter, name=f"ref_set_pol_state{polname}"):
+        for h, s in enumerate(
+            [
+                PhswPinMode.SLOW_SWITCHING_FORWARD,
+                PhswPinMode.SLOW_SWITCHING_REVERSE,
+                PhswPinMode.FAST_SWITCHING_FORWARD,
+                PhswPinMode.FAST_SWITCHING_REVERSE,
+            ]
+        ):
+            self.conn.set_phsw_status(polarimeter=polname, phsw_index=h, status=s)
+
+    self.conn.set_hk_scan(boards=cur_board, allboards=False, time_ms=500)
+
+    wait_with_tag(conn=self.conn, seconds=120, name=f"ref_acquisition_pol{polname}")
