5cd242a1d63cea91c45e7021b97025d832356feb
Maurizio Tomasi <ziotom78@gmail.com>
2021-11-19 12:18:40 +0100

    Improve error handling in fix_hdf5.py

--- fix_hdf5.py
+++ fix_hdf5.py
@@ -12,6 +12,25 @@ from striptease import (
 )
 
 
+def fix_file(hdf5_file: str) -> bool:
+    """Fix FIRST_SAMPLE and LAST_SAMPLE in a HDF5.
+
+    Return False if there was no need to fix them."""
+
+    with h5py.File(hdf5_file, mode="r+") as h5_file:
+        min_mjd = h5_file.attrs.get("FIRST_SAMPLE", -1)
+        max_mjd = h5_file.attrs.get("LAST_SAMPLE", -1)
+
+        if (min_mjd > 0) and (max_mjd > min_mjd):
+            return False
+
+        min_mjd, max_mjd = find_first_and_last_samples_in_hdf5(h5_file)
+        h5_file.attrs["FIRST_SAMPLE"] = min_mjd
+        h5_file.attrs["LAST_SAMPLE"] = max_mjd
+
+    return True
+
+
 def main(args):
     if len(args) == 1:
         print(
@@ -34,19 +53,15 @@ you see errors related to «lock» problems, set the environment variable
     messages = []
     for cur_file in tqdm(args[1:]):
         cur_file = Path(cur_file)
-        with h5py.File(cur_file, mode="r+") as h5_file:
-            min_mjd = h5_file.attrs.get("FIRST_SAMPLE", -1)
-            max_mjd = h5_file.attrs.get("LAST_SAMPLE", -1)
-
-            if (min_mjd > 0) and (max_mjd > min_mjd):
+        try:
+            if fix_file(cur_file):
+                messages.append(f'File "{cur_file}" has been fixed')
+            else:
                 messages.append(f'File "{cur_file}" looks ok, skipping…')
-                continue
-
-            min_mjd, max_mjd = find_first_and_last_samples_in_hdf5(h5_file)
-            h5_file.attrs["FIRST_SAMPLE"] = min_mjd
-            h5_file.attrs["LAST_SAMPLE"] = max_mjd
-
-            messages.append(f'File "{cur_file}" has been fixed')
+        except OSError as e:
+            messages.append(f'File "{cur_file}" could not be fixed (OSError): {e}')
+        except RuntimeError as e:
+            messages.append(f'File "{cur_file}" could not be fixed (RuntimeError): {e}')
 
     for msg in messages:
         print(msg)
